bf8d53ebb36d3b2fd1aa973a39163353d176b17b
Hirofumi SHINKE <hiro@molloy.local>
2022-04-06 00:04:03 +0900

    use curl to install antlr4

--- antlr.sh
+++ antlr.sh
@@ -1,6 +1,11 @@
+#!/bin/sh
 
+[ -f antlr-4.9.2-complete.jar ] || \
+    curl -O https://www.antlr.org/download/antlr-4.9.2-complete.jar
 
-java -jar /Users/hiro/.m2/repository/org/antlr/antlr4/4.9.3-SNAPSHOT/antlr4-4.9.3-SNAPSHOT-complete.jar src/main/antlr4/com/github/hiroshinke/antlrsample/Arithmetic.g4 -Dlanguage=Python3 -visitor -o dist
+export CLASSPATH=".:antlr-4.9.2-complete.jar:$CLASSPATH"
+alias antlr4='java -jar antlr-4.9.2-complete.jar'
+alias grun='java org.antlr.v4.gui.TestRig'
 
-
+antlr4 src/main/antlr4/com/github/hiroshinke/antlrsample/Arithmetic.g4 -Dlanguage=Python3 -visitor -o dist
 
